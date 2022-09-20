9ba2d2ccf9a5f07b544ff9d3e1ea57cd5213469c
NfoTECH <fortunenwachukwu@gmail.com>
2022-09-20 23:07:09 +0100

    ShortenPathChallenge added

--- src/stack/ShortenPathChallenge.java
+++ src/stack/ShortenPathChallenge.java
@@ -1,2 +1,34 @@
-package stack;public class ShortenPathChallenge {
+package stack;
+
+import java.util.Stack;
+
+public class ShortenPathChallenge {
+    public static String shortenPath(String path) {
+        String result = "";
+        String[] arr = path.split("/");
+        Stack<String> stack = new Stack<>();
+        for (String s : arr) {
+            if (s.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else if (!s.equals(".") && !s.equals("")) {
+                stack.push(s);
+            }
+        }
+        if (stack.isEmpty()) {
+            return "/";
+        }
+        while (!stack.isEmpty()) {
+            result = "/" + stack.pop() + result;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(shortenPath("/foo/../test/../test/../foo//bar/./baz"));
+        System.out.println("/foo/bar/baz");
+        System.out.println("/../../foo/bar/baz");
+        System.out.println("/../../foo/../../bar/baz");
+    }
 }
