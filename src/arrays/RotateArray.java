c293995f7a31dfe183de30fc9251d9f7354d52ec
NfoTECH <fortunenwachukwu@gmail.com>
2022-09-20 23:04:33 +0100

    RotataArray Challenge modified

--- src/arrays/RotateArray.java
+++ src/arrays/RotateArray.java
@@ -5,16 +5,16 @@ import java.util.Collections;
 
 public class RotateArray {
     public static void main(String[] args) {
-        Integer [] arr = {4,5, 6};
+        Integer [] arr = {1,2,3,4,5,6,7};
         Integer [] arr2 = {4,5, 6};
 
         Collections.rotate(Arrays.asList(arr2), -1); // Left rotation
-        Collections.rotate(Arrays.asList(arr), 1); // Right rotation
+        Collections.rotate(Arrays.asList(arr), 3); // Right rotation
 
 
 
 
-        System.out.println(Arrays.toString(arr2));
+        //System.out.println(Arrays.toString(arr2));
         System.out.println(Arrays.toString(arr));
     }
 }
