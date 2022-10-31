850978537a38eb9873185ca15f533cc3c4457987
NfoTECH <fortunenwachukwu@gmail.com>
2022-10-31 10:02:57 +0100

    merging and sorting two given arrays

---
+++ src/assessmentDay/MergeSortAlgorithm.java
@@ -0,0 +1,41 @@
+package assessmentDay;
+
+
+public class MergeSortAlgorithm {
+    public static int[] mergeSort(int[] arr1, int[] arr2) {
+        int[] result = new int[arr1.length + arr2.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                result[k] = arr1[i];
+                i++;
+            } else {
+                result[k] = arr2[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < arr1.length) {
+            result[k] = arr1[i];
+            i++;
+            k++;
+        }
+        while (j < arr2.length) {
+            result[k] = arr2[j];
+            j++;
+            k++;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {3,4,9,10,11};
+        int[] arr2 = {99,100};
+        int[] result = mergeSort(arr1, arr2);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+}
