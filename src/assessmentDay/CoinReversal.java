077246e3634c975b528744d05b17043a81b431ff
NfoTECH <fortunenwachukwu@gmail.com>
2022-11-18 11:39:09 +0100

    Coin reversal

--- src/assessmentDay/CoinReversal.java
+++ src/assessmentDay/CoinReversal.java
@@ -13,19 +13,43 @@ package assessmentDay;//There are N coins, each showing either heads or tails. W
 //        2. Given array A = [1, 1, 0, 1, 1], the function should return 2. After reversing the first and fourth coins, we achieve an alternating sequence of coins [0,1,0,0,1].
 
 public class CoinReversal {
+    public static int solution(int[] A) {
+        // write your code in Java SE 8
+        int result = 0;
+        int count = 0;
+        int count1 = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (i % 2 == 0) {
+                if (A[i] == 1) {
+                    count++;
+                }
+            } else if (i % 2 != 0) {
+                if (A[i] == 0) {
+                    count++;
+                }
+            }
+        }
+        for (int i = 0; i < A.length; i++) {
+            if (i % 2 == 0) {
+                if (A[i] == 0) {
+                    count1++;
+                }
+            } else if (i % 2 != 0) {
+                if (A[i] == 1) {
+                    count1++;
+                }
+            }
+        }
+        return Math.min(count, count1);
+    }
+
+
     public static void main(String[] args) {
         int[] A = {1, 0, 1, 0, 1, 1};
         int[] B = {1, 1, 0, 1, 1};
+        int[] C = {0, 1, 1, 0};
         System.out.println(solution(A));
         System.out.println(solution(B));
-    }
-    public static int solution(int[] A) {
-        int count = 0;
-        for (int i = 0; i < A.length - 1; i++) {
-            if (A[i] == A[i + 1]) {
-                count++;
-            }
-        }
-        return count;
+        System.out.println(solution(C));
     }
 }
