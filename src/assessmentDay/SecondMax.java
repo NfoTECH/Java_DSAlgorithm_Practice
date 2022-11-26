a2d90f056be3b9ecca7322c75b3d4fe4d2e6d78a
NfoTECH <fortunenwachukwu@gmail.com>
2022-11-26 21:00:38 +0100

    second max challenge modified

--- src/assessmentDay/SecondMax.java
+++ src/assessmentDay/SecondMax.java
@@ -1,5 +1,7 @@
 package assessmentDay;
 
+import java.util.Arrays;
+
 public class SecondMax {
     public int returnSecondMax(int[] nums) {
         int max = Integer.MIN_VALUE;
@@ -14,9 +16,19 @@ public class SecondMax {
         }
         return secondMax;
     }
+    public int returnSecondMax2(int[] nums) {
+        Arrays.sort(nums);
+        if (nums.length > 1) {
+            return nums[nums.length - 2];
+        } else {
+            return nums[0];
+        }
+    }
 
-    public static void main(String[] args) {
-        SecondMax p = new SecondMax();
-        System.out.println(p.returnSecondMax(new int[]{1,2,3,4,5,6,7,8,9,10}));
+public static void main(String[] args) {
+        SecondMax sm = new SecondMax();
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 77, 76, 10};
+        System.out.println(sm.returnSecondMax(nums));
+        System.out.println(sm.returnSecondMax2(nums));
     }
 }
