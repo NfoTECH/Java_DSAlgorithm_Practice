9be9abbe3454670f71afd8c0d99d438144264d60
NfoTECH <fortunenwachukwu@gmail.com>
2022-11-30 20:52:38 +0100

    Get minimum operations challenge solved

---
+++ src/assessmentDay/GetMinOperations.java
@@ -0,0 +1,69 @@
+package assessmentDay;
+
+//Given a string that consists of only two types of characters: '(' and ')', balance the parentheses by inserting either a '(' or a ')' as many times as necessary. Determine the minimum number of characters that must be inserted.
+//
+//        Example
+//
+//        s="(0))"
+//
+//        To make it a valid sequence, insert a '('at the beginning of the string, resulting in "((()))". The string is balanced after 1 insertion.
+//
+//        Function Description
+//
+//        Complete the function getMinOperations in the editor below. The function must return the minimum number of operations needed to make the parentheses sequence valid.
+//
+//        getMinOperations has the following parameter(s): string s: a string of parentheses
+//
+//        Return
+//
+//        int: the minimum number of insertions required to balance the parentheses
+//
+//        Constraints
+//
+//        • 1 s length of ss 105
+//
+//        .
+//
+//        ► Input Format For Custom Testing
+//
+//        ▾ Sample Case 0
+//
+//        Sample Input For Custom Testing
+//
+//        STDIN
+//
+//        Function
+//
+//        ())) → s = '()))'
+//
+//        Sample Output
+//
+//        2
+//
+//        Explanation
+//
+//        Insert a '(' 2 times at the beginning of the string to make it valid: "((0))"
+
+public class GetMin {
+    public static int getMinOperations(String s) {
+        int count = 0;
+        int open = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                open++;
+            } else {
+                if (open == 0) {
+                    count++;
+                } else {
+                    open--;
+                }
+            }
+        }
+        return count + open;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMinOperations("()))"));
+        System.out.println(getMinOperations("()()"));
+    }
+}
