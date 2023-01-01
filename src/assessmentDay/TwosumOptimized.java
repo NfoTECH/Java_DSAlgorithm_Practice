be2d8ae4abf1ee9cb35a5357390dff88187fbc0d
NfoTECH <fortunenwachukwu@gmail.com>
2023-01-01 08:43:22 +0100

    Optimized two sum solution

--- src/assessmentDay/TwosumOptimized.java
+++ src/assessmentDay/TwosumOptimized.java
@@ -38,4 +38,28 @@ public class TwosumOptimized {
             System.out.println(result[i]);
         }
     }
+
+
+    //IF THE INPUT ARRAY IS SORTED IN ASC ORDER, WE USE BINARY SEARCH
+    //TIME COMPLEXITY O(N Log N), SPACE COMPLEXITY O(1)
+    public int[] twoSum3(int[] numbers, int target) {
+// Assume input is already sorted.
+        for (int i = 0; i < numbers.length; i++) {
+            int j = bSearch(numbers, target - numbers[i], i + 1);
+            if (j != -1) {
+                return new int[] { i + 1, j + 1 };
+            } }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+    private int bSearch(int[] A, int key, int start) {
+        int L = start, R = A.length - 1;
+        while (L < R) {
+            int M = (L + R) / 2;
+            if (A[M] < key) {
+                L = M + 1;
+            } else {
+                R = M;
+            } }
+        return (L == R && A[L] == key) ? L : -1;
+    }
 }
