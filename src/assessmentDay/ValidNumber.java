35a565a575bce9221914d14372411ff2de584141
NfoTECH <fortunenwachukwu@gmail.com>
2023-01-02 22:18:57 +0100

    check for valid number solution

---
+++ src/assessmentDay/ValidNumber.java
@@ -0,0 +1,23 @@
+package assessmentDay;
+
+public class ValidNumber {
+    public boolean isNumber(String s) {
+        int i = 0, n = s.length();
+        while (i < n && Character.isWhitespace(s.charAt(i))) i++;
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) i++;
+        boolean isNumeric = false;
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            i++;
+            isNumeric = true;
+        }
+        if (i < n && s.charAt(i) == '.') {
+            i++;
+            while (i < n && Character.isDigit(s.charAt(i))) {
+                i++;
+                isNumeric = true;
+            }
+        }
+        while (i < n && Character.isWhitespace(s.charAt(i))) i++;
+        return isNumeric && i == n;
+    }
+}
