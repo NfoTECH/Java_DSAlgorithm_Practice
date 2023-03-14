a1c5390ebd488f1e1d767b92e69b7e2713306904
NfoTECH <fortunenwachukwu@gmail.com>
2023-03-15 00:21:48 +0100

    Solve the Sum Pair to target challenge

--- src/assessmentDay/FindSumPair.java
+++ src/assessmentDay/FindSumPair.java
@@ -1,10 +1,10 @@
 package assessmentDay;
 
-//In this problem, you'll be given a list of positive integers and a separate integer, and casked with finding whether there is a pair of integers in the list that sum to exactly
+//In this problem, you'll be given a list of positive integers and a separate integer, and tasked with finding whether there is a pair of integers in the list that sum to exactly
 //
 //        Implementation
 //
-//        Implement the method findair (mbers, ky which takes as input
+//        Implement the method findSumPair (numbers, ky which takes as input
 //
 //        an array of positive integers (numbers)
 //
@@ -16,17 +16,12 @@ package assessmentDay;
 //
 //        13
 //
-//        Your EPALE method should return an array of two integers, containing the indices of a pair of interste array that sum to Note that
+//        Your findSumPair method should return an array of two integers, containing the indices of a pair of intersect array that sum to Note that
 //
 //        The first index of the array is 0
 //
-//        The first integer in your output should represent the lower index 10. 01 should be returned if there no pair is found
-//
-//        In the case that there are multiple possible pairs that sum to the target return me paranose ist edes the
-//
-//        lowest In the case of two pairs having the same lett vides, favor the pair with the tower right indes
-//
-//        For the above example, the correct output would be 11, 31
+//        The first integer in your output should represent the lower index 10. 01 should be returned if there is no pair is found
+
 
 import java.util.Arrays;
 
