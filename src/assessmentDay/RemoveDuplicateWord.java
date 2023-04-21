6cf3ca0f77b13c7c8181abd2ed0cb7d3ca154bcd
NfoTECH <fortunenwachukwu@gmail.com>
2023-04-21 07:38:14 +0100

    remove duplicate words in a sentence

---
+++ src/assessmentDay/RemoveDuplicateWord.java
@@ -0,0 +1,21 @@
+package assessmentDay;
+
+//String str = "Rose rose is red";
+// Expected output = "Rose is red"
+// Remove duplicate words from the string and take care of the case sensitivity
+public class RemoveDuplicateWord {
+    public static void main(String[] args) {
+        String str = "Rose rose is red";
+        System.out.println(removeDuplicateWords(str));
+    }
+
+    static String removeDuplicateWords(String str) {
+        str = str.toLowerCase();
+        String[] words = str.split(" ");
+        String result = "";
+        for (int i = 0; i < words.length; i++) {
+            if (!result.contains(words[i])) result += words[i] + " ";
+        }
+        return result;
+    }
+}
