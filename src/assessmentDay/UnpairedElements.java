7dcfc9fc0285af1b08c4c09b8996c1398202b0a3
NfoTECH <fortunenwachukwu@gmail.com>
2023-06-24 19:06:48 +0100

    Update: Return the elements that have no match or pair in the list

--- src/assessmentDay/UnpairedElements.java
+++ src/assessmentDay/UnpairedElements.java
@@ -2,8 +2,8 @@ package assessmentDay;
 
 import java.util.ArrayList;
 import java.util.List;
-//A non-empty array A consisting of N integers is given. The array contains an odd number of elements, and each element of the array can be paired with another element that has the same value, except for one element that is left unpaired.
-//
+//A non-empty array A consisting of N integers is given. The array contains an odd number of elements,
+// and each element of the array can be paired with another element that has the same value, except for one element that is left unpaired.
 //        For example, in array A such that:
 //
 //        A[0] = 9  A[1] = 3  A[2] = 9
